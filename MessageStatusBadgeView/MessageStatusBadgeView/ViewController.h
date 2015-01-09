8462b63bda4c4b14c6ddd8ec805b5c171a852d03
Ravi Prakash <ravi@helpshift.com>
2015-01-09 16:28:03 +0530

    Init commmit

---
+++ MessageStatusBadgeView/MessageStatusBadgeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MessageStatusBadgeView
+//
+//  Created by Ravi Prakash on 09/01/15.
+//  Copyright (c) 2015 OpenSource. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MessageStatusBadgeView/MessageStatusBadgeView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MessageStatusBadgeView
+//
+//  Created by Ravi Prakash on 09/01/15.
+//  Copyright (c) 2015 OpenSource. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
